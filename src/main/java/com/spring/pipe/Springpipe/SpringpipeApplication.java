aa422008e1b0d79da6d593c3be0d057a7f93d277
student <yash@avotrix.com>
2021-06-19 17:01:29 +0000

    Maven Spring project from spring initializer

---
+++ src/main/java/com/spring/pipe/Springpipe/SpringpipeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.pipe.Springpipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/pipe/Springpipe/SpringpipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pipe.Springpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
